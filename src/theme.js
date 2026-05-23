f26588717f3da944318e58f2de22cff1147d33d1
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-05-23 13:39:17 +0300

    feat: configure antd global theme

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,22 @@
 export const theme = {
-    token: {
-        colorPrimary: "#e53935",
+  token: {
+    colorPrimary: "#1677ff",
+    borderRadius: 6,
+  },
+  components: {
+    Button: {
+      borderRadius: 32,
+      borderRadiusLG: 32,
+      borderRadiusSM: 32,
     },
-    components: {
-        Button: {
-            borderRadius: 0,
-            borderRadiusLG: 0,
-            borderRadiusSM: 0,
-        },
+    Table: {
+      headerBg: "#1f1f1f",
+      headerColor: "#ffffff",
+      headerSortActiveBg: "#2d2d2d",
+      headerSortHoverBg: "#262626",
+      rowHoverBg: "#f5f5f5",
+      padding: 12,
+      paddingContentVertical: 10,
     },
+  },
 };
