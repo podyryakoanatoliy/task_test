4000a689946f51c3d9548e5240744e4cdc6067c8
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-05-23 16:29:16 +0300

    feat: realised coinPage/task2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,13 @@
-import { HomeOutlined } from "@ant-design/icons";
+import { HomeOutlined, DollarOutlined } from "@ant-design/icons";
 import Home from "./pages/Home.jsx";
+import Coins from "./pages/Coins.jsx";
 
 export const routes = [
-    { path: "/", label: "Home", icon: <HomeOutlined />, element: <Home /> },
+  { path: "/", label: "Home", icon: <HomeOutlined />, element: <Home /> },
+  {
+    path: "/coins",
+    label: "Coin",
+    icon: <DollarOutlined />,
+    element: <Coins />,
+  },
 ];
