09cdf409e3e52389cccb461788e0a8f6e30222ff
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-05-23 13:10:00 +0300

    test commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,21 @@ import { Card, Typography } from "antd";
 const { Title, Paragraph, Link } = Typography;
 
 function Home() {
-    return (
-        <Card>
-            <Title level={2}>Тестове завдання</Title>
-            <Paragraph>
-                Це стартовий шаблон. Усі завдання описані у файлі{" "}
-                <Link href="/TASK.md" target="_blank">
-                    TASK.md
-                </Link>{" "}
-                у корені проєкту.
-            </Paragraph>
-            <Paragraph type="secondary">
-                Стек: React + Vite + Ant Design + React Router.
-            </Paragraph>
-        </Card>
-    );
+  return (
+    <Card>
+      <Title level={2}>Тестове завдання</Title>
+      <Paragraph>
+        Це стартовий шаблон. Усі завдання описані у файлі Just a test commit
+        <Link href="/TASK.md" target="_blank">
+          TASK.md
+        </Link>{" "}
+        у корені проєкту.
+      </Paragraph>
+      <Paragraph type="secondary">
+        Стек: React + Vite + Ant Design + React Router.
+      </Paragraph>
+    </Card>
+  );
 }
 
 export default Home;
